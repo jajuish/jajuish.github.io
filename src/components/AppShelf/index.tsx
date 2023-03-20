4b5f4b0d94469c2aaad6ca31f336728c98e1b857
Ishita Jaju <ishijaju@gmail.com>
2023-03-20 17:21:49 +0530

    Scroller auto find implementation

--- src/components/AppShelf/index.tsx
+++ src/components/AppShelf/index.tsx
@@ -7,22 +7,9 @@ interface IAppShelf {
   children: JSX.Element[];
 }
 export default function AppShelf({ children }: IAppShelf) {
-  const [selectedItem, setSelectedItem] = useState<number>(0);
-  const handleClickScroll = (num: number) => {
-    const element = document.getElementById(pageElementIdGenerator(num));
-    setSelectedItem(num)
-    if (element) {
-      // 👇 Will scroll smoothly to the top of the next section
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
     <div>
-      <Scroller
-        handleClickScroll={handleClickScroll}
-        selectedItem={selectedItem}
-      >
+      <Scroller>
         {children.map((child, i) =>
           <div
             id={pageElementIdGenerator(i)}
--- src/components/Scroller/index.tsx
+++ src/components/Scroller/index.tsx
@@ -1,41 +1,23 @@
-import { useEffect, useMemo, useRef, useState, RefObject, createRef } from "react";
+import { useEffect, useMemo, useRef, useState, useLayoutEffect } from "react";
+
 import { Circle } from "../../assets";
+import { pageElementIdGenerator } from '../../utils/helpers';
 import "./styles.scss";
 
 interface IScroller {
 	children: JSX.Element[];
-	handleClickScroll: (i: number) => void;
-	selectedItem: number;
 }
-export default function Scroller({ children, handleClickScroll, selectedItem }: IScroller) {
-	const positions = useMemo(() =>
-		children.reduce(
-			(_positions, _children) => {
-				// console.log("_positions==",_positions)
-				// console.log(_children)
-				const lastElement = _positions.slice(-1)[0];
-				const height = _children.props.height
-					? parseInt(_children.props.height)
-					: 100;
-				return _positions.concat([lastElement - height]);
-			},
-			[0],
-		),
-		[children],
-	);
-	// console.log(positions)
-
-	const getPagesNumbers = () => {
-		const pageNumbers = [];
+export default function Scroller({ children }: IScroller) {
+	const createNavigator = () => {
+		const navigationPoints = [];
 
 		for (let i = 0; i < children.length; i++) {
-			pageNumbers.push(
+			navigationPoints.push(
 				<div
 					key={i}
-					onClick={() => handleClickScroll(i)}
+					onClick={() => handleClick(i)}
 					className="navigation-button"
 				>
-					{/* {i + 1} */}
 					<Circle
 						// size={paginationCircleSize.medium}
 						active={i === selectedItem}
@@ -46,47 +28,58 @@ export default function Scroller({ children, handleClickScroll, selectedItem }: 
 			);
 		}
 
-		return [...pageNumbers];
+		return [...navigationPoints];
 	};
 
-	const ref1 = useRef<HTMLDivElement>(null);
-	const isInViewport1 = useIsInViewport(ref1);
-	console.log('isInViewport1: ', isInViewport1);
+	const [selectedItem, setSelectedItem] = useState<number>(0);
+  const handleClick = (num: number) => {
+    const element = document.getElementById(pageElementIdGenerator(num));
+    setSelectedItem(num)
+    if (element) {
+      // 👇 Will scroll smoothly to the top of the next section
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+	const elRefs = useRef<Array<HTMLDivElement | null>>([])
+	const [componentPositions, setComponentPositions] = useState<number[]>([])
+	useEffect(() => {
+		const componentHeights: number[] = children.map((e, i) => elRefs.current[i]?.clientHeight || 0)
+		setComponentPositions(componentHeights.reduce((_height, _children) => {
+			const lastElement = _height.slice(-1)[0];
+			return _height.concat([lastElement + _children]);
+		},
+			[0],
+		))
+	}, [elRefs, children])
+
+	const handleScroll = () => {
+		const position = window.pageYOffset;
+	const currComp = componentPositions.concat(position).sort((a, b) => a - b).indexOf(position);
+		setSelectedItem((currComp.valueOf())-1)
+	};
+
+	useLayoutEffect(() => {
+		window.addEventListener('scroll', handleScroll, { passive: false });
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 
 	return (
 		<>
 			<div className="pagination">
-				{getPagesNumbers()}
+				{createNavigator()}
 			</div>
-			{children}
-			<div
-				style={{ backgroundColor: "blue", height: 200, width: 200 }}
-				ref={ref1}
-			/>
+			{children.map((childElement, i) =>
+				<div
+					ref={el => elRefs.current[i] = el}
+					key={i}
+				>
+					{childElement}
+				</div>
+			)}
 		</>
 	)
-}
-
-function useIsInViewport(ref: React.MutableRefObject<HTMLDivElement | null> | null) {
-	// console.log("REF==",ref?.current)
-	const [isIntersecting, setIsIntersecting] = useState(false);
-
-	const observer = useMemo(
-		() =>
-			new IntersectionObserver(([entry]) =>
-				setIsIntersecting(entry.isIntersecting),
-			),
-		[],
-	);
-
-	useEffect(() => {
-		if (ref !== null && ref.current !== null)
-			observer.observe(ref.current);
-
-		return () => {
-			observer.disconnect();
-		};
-	}, [ref?.current, observer]);
-
-	return isIntersecting;
 }
